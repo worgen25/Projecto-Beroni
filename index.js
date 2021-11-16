6ec4f8d5969d2208d7c0aa15ac4d116049d08ea6
Jhon castaño <jhon596@outlook.com>
2021-11-16 10:50:51 -0500

    s s with '#' will be ignored, and an empty message aborts the commit.

---
+++ index.js
@@ -0,0 +1,11 @@
+let x = document.getElementById("eleccion1");
+
+function myfuncion() {
+    x.style.opacity="1";
+    x.style.zIndex="0";
+}
+
+function myFuncion2() {
+    x.style.opacity="0";
+    x.style.zIndex="-2";
+}
